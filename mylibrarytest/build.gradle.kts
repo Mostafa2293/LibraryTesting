74880a2e8b0a1ab72bcd6c1a04371676867dc6d6
mostafa.abdelsayed <mostafa.abdelsayed@btech.com>
2023-10-03 15:05:51 +0300

    3rd fun

--- mylibrarytest/build.gradle.kts
+++ mylibrarytest/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
 
                 groupId = "com.github.Mostafa"
                 artifactId = "libraryTest"
-                version = "3.0"
+                version = "4.0"
             }
         }
     }
--- mylibrarytest/src/main/java/com/example/mylibrarytest/LibraryTest.kt
+++ mylibrarytest/src/main/java/com/example/mylibrarytest/LibraryTest.kt
@@ -3,5 +3,6 @@ package com.example.mylibrarytest
 class LibraryTest {
     fun hello(name:String) {}
     fun hello2(name:String,id:Int,hamada:String) {}
+    fun hello3(name:String,id:Int,hamada:String) {}
 
 }
