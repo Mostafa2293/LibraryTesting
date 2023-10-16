88b8cf082c8d8b68a622f46936390d08164c2be6
mostafa.abdelsayed <mostafa.abdelsayed@btech.com>
2023-10-16 11:39:59 +0300

    v7

--- mylibrarytest/build.gradle.kts
+++ mylibrarytest/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URL
+import java.io.FileOutputStream
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -33,6 +35,33 @@ android {
     }
 }
 
+open class DownloadTokensJson : DefaultTask() {
+
+    @TaskAction
+    fun downloadFile() {
+        val url =
+            URL("https://raw.githubusercontent.com/Andrew2120/fiber-core/main/Tokens.kt")
+        val outputDir = File("mylibrarytest/src/main/java/com/example/mylibrarytest") // Specify the assets folder
+        val outputFile = File(outputDir, "Tokens.kt")
+        val connection = url.openConnection()
+        connection.connect()
+        val inputStream = connection.getInputStream()
+
+        val outputStream = FileOutputStream(outputFile)
+        val buffer = ByteArray(4096)
+        var bytesRead: Int
+        while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+            outputStream.write(buffer, 0, bytesRead)
+        }
+
+        inputStream.close()
+        outputStream.close()
+    }
+}
+tasks.register<DownloadTokensJson>("DownloadTokens")
+
+tasks.getByPath("preBuild").dependsOn("DownloadTokens")
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -41,6 +70,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.beust:klaxon:5.5")
 }
 afterEvaluate {
     publishing {
@@ -50,7 +80,7 @@ afterEvaluate {
 
                 groupId = "com.github.Mostafa"
                 artifactId = "libraryTest"
-                version = "6.0"
+                version = "7.0"
             }
         }
     }
